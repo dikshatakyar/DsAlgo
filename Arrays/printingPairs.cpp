8975b9a2c453be485a8df2fb31674fe5162c7837
Diksha Takyar <diksha.takyar@gmail.com>
2022-06-12 12:49:03 +0530

    all pairs

---
+++ Arrays/printingPairs.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int arr[] = {1, 2, 3, 4};
+    int size = sizeof(arr)/sizeof(int);
+    for(int i = 0; i < size ; ++i){
+        for(int j = i + 1; j < size; ++j){ //j<=size
+            cout << arr[i] <<","<< arr[j] << endl;
+        }
+        cout << endl;
+    }
+
+}
