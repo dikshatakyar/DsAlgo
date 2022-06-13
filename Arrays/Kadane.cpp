eb75507836b74325258246d371d34375883f0edf
Diksha Takyar <diksha.takyar@gmail.com>
2022-06-13 21:14:39 +0530

    all val neg

--- Arrays/Kadane.cpp
+++ Arrays/Kadane.cpp
@@ -21,3 +21,32 @@ int main(){
       printsubarrays(arr, n);
 
 }
+
+
+/* in case all values are negative in an array
+#include<bits/stdc++.h>
+using namespace std;
+
+int maxSumSubarray(vector<int> A) {
+    int curr_sum = 0;
+    int maxm_sum = 0;
+    
+  	bool pos = false;
+  	int max_num = A[0];
+  
+    for(int i = 0; i < A.size(); ++i){
+        curr_sum += A[i];
+        if(curr_sum < 0){
+            curr_sum = 0;
+        }
+      	if(A[i] > 0) pos = true;
+        max_num = max(max_num, A[i]); 
+        maxm_sum = max(curr_sum, maxm_sum);
+    }
+  	
+  	if(pos == false){
+      return max_num;
+    }
+    return maxm_sum;
+}
+*/
