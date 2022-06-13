d02a81765dacd949287b11f28c334c6d4b0cd5ab
Diksha Takyar <diksha.takyar@gmail.com>
2022-06-13 21:14:04 +0530

    update

--- Arrays/printingPairs.cpp
+++ Arrays/printingPairs.cpp
@@ -5,10 +5,10 @@ int main(){
     int arr[] = {1, 2, 3, 4};
     int size = sizeof(arr)/sizeof(int);
     for(int i = 0; i < size ; ++i){
-        for(int j = i + 1; j < size; ++j){ //j<=size
+        for(int j = i + 1; j < size; ++j){ 
             cout << arr[i] <<","<< arr[j] << endl;
         }
         cout << endl;
     }
-
+    
 }
