2e533fe2c08d23a93607edae9447af8c13baf34a
Diksha Takyar <diksha.takyar@gmail.com>
2022-06-15 10:50:54 +0530

    k-rotate

---
+++ Arrays/Krotate.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+void reverse(int *arr, int start, int end){
+    
+    	int l = start, r = end;
+      
+      	while(l < r){
+          	swap(arr[l], arr[r]);
+          	l++; r--;
+        }
+}
+
+vector<int> kRotate(vector<int> a, int k){
+    // your code  goes here
+    int n = a.size();
+    reverse(a, 0, k-1);
+    reverse(a, k, n-1);
+    reverse(a, 0, n-1);
+    
+}
+
