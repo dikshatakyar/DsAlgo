62576af33c791b12b727824cf571a4d6906a18bc
Diksha Takyar <diksha.takyar@gmail.com>
2022-07-09 22:42:11 +0530

    pehli baar

---
+++ Recursion/firstOccurence.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int firstOcc(int *arr, int n, int key){
+    if(n == 0){
+        return -1;
+    }
+    if(key == arr[0]){
+        return 0;
+    }
+    int subIndex = firstOcc(arr + 1, n - 1, key);
+    if(subIndex != -1){
+        return subIndex + 1;
+    }
+    return -1;
+}
+
+
+int main(){
+    int arr[] = {1,3,5,8,7,6,2,11,21};
+    int n = sizeof(arr)/sizeof(int);
+    int key = 7;
+    cout << firstOcc(arr, n, key);
+}
