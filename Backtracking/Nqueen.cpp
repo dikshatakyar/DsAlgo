77059fdc3893d3b1bcf51272fb7c231f2ff4b174
Diksha Takyar <diksha.takyar@gmail.com>
2022-07-23 22:48:35 +0530

    queeeeen

---
+++ Backtracking/Nqueen.cpp
@@ -0,0 +1,67 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void print(int board[][20], int n){
+    for(int i = 0; i < n; ++i){
+        for(int j = 0; j < n; ++j){
+            cout << board[i][j] <<" ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
+
+bool canPlace(int board[][20], int n, int i, int j){
+     
+     //checking if any queen was placed in the same column : col remain constant, row changes
+     for(int row = 0; row < i; ++row){
+        if(board[row][j] == 1){
+            return false;
+        }
+     }
+     //checking for left diagonal
+     while(i >= 0 && j >=0){
+        if(board[i][j] == 1){
+            return false;
+        }
+        --i, --j;
+     }
+     //checking for right diagonal
+     while(i >= 0 && j < n){
+        if(board[i][j] == 1){
+            return false;
+        }
+        --i, ++j;
+     }
+
+    return true;
+}
+
+bool FindNqueen(int board[][20], int n, int i){
+    if(i == n){
+        print(board, n);
+        return true;
+    }
+    //for every row, we try to place the queen in every column 
+    for(int j = 0; j < n; ++j){
+        if(canPlace(board, n, i, j)){
+            board[i][j] = 1; //placing the queen
+            bool success = FindNqueen(board, n, i + 1); //go to the next row
+            if(success){
+                return true;
+            }
+            //backtrack
+            board[i][j] = 0; //removing the queen;
+        }
+    }
+    return false; //after finishing entire row(all cols have been checked in the current row), if the queen could not be placed, then we tell previous row something is wrong with your configuration
+
+}
+
+int main(){
+    int board[20][20] = {0};
+    int n;
+    cin >> n;
+    int i = 0;
+    FindNqueen(board, n, 0);
+}
